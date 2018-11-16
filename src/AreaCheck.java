c2f60c6dddfd6e4374add5c3070d774225d8ec62
Arseny <ar.poryadin@yandex.ru>
2018-11-16 17:29:05 +0300

    Added AreaCheck & fucked-up main page

---
+++ src/AreaCheck.java
@@ -0,0 +1,34 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class AreaCheck {
+    private int x;
+    private double y;
+    private double r;
+
+    public int getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+
+}
